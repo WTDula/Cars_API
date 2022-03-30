fb947e1ea29895020fd8e159e96d1c340e9c653b
WTDula <wtdula@gmail.com>
2022-03-30 16:18:31 -0500

    completed POST functionality, updated rest framework decimal settings

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,22 @@
+from urllib import response
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Car
 from .serializers import CarSerializer
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def cars_list(request):
-    cars = Car.objects.all()
 
-    serializer = CarSerializer(cars, many=True)
+    if(request.method == 'GET'):
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data)
 
-    return Response(serializer.data)
+
+    elif(request.method == "POST"):
+        serializer = CarSerializer(data=request.data)
+        if(serializer.is_valid() == True):
+            serializer.save()
+            return Response(serializer.data, status = 201)
+        else:
+            return Response(serializer.errors, status = 400)
